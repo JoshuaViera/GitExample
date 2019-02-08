7ef6e54a91a6dc466701acc1a27651396b70b4c7
JoshuaViera <joshuaviera95@gmail.com>
2019-02-08 10:54:58 -0500

    Initial Commit

---
+++ GitExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExamples
+//
+//  Created by Joshua Viera on 2/8/19.
+//  Copyright © 2019 Joshua Viera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Working On Git")
+    }
+
+
+}
+
